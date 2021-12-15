bc2104ca97a1335466b73f283f61c8d6ca840e66
vstudioac12@gmail.com <vstudioac12@gmail.com>
2021-12-15 18:18:45 +0200

    Add project files.

---
+++ proooooject/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace proooooject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_upadate = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_search = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(12, 353);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 0;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_upadate
+            // 
+            this.btn_upadate.Location = new System.Drawing.Point(111, 353);
+            this.btn_upadate.Name = "btn_upadate";
+            this.btn_upadate.Size = new System.Drawing.Size(75, 23);
+            this.btn_upadate.TabIndex = 1;
+            this.btn_upadate.Text = "Upadate";
+            this.btn_upadate.UseVisualStyleBackColor = true;
+            this.btn_upadate.Click += new System.EventHandler(this.btn_upadate_Click);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(210, 353);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(75, 23);
+            this.btn_del.TabIndex = 2;
+            this.btn_del.Text = "Delete";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(325, 137);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(392, 276);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "EMPNO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "DEPTNO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "NAME";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 254);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "JOB";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(102, 158);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(102, 200);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(102, 251);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(384, 83);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(252, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(292, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "EMPLOYE NUM";
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(642, 83);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(75, 23);
+            this.btn_search.TabIndex = 14;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 425);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.btn_upadate);
+            this.Controls.Add(this.btn_Add);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_upadate;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_search;
+    }
+}
+
---
+++ proooooject/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+namespace proooooject
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Server=.; DataBase=EmpInfo;Initial Catalog= EmpInfo ; Integrated Security=true");
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+        DataTable Dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+            {
+                cmd = new SqlCommand("insert into emp(DeptNO,Name,job) values(@DeptNo,@Name,@job)", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@DeptNo", textBox1.Text);
+
+                // cmd.Parameters.AddWithValue("@empNo", textBox2.Text);
+                cmd.Parameters.AddWithValue("@Name", textBox3.Text);
+                cmd.Parameters.AddWithValue("@job", textBox4.Text);
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record Inserted Successfully");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            }
+
+        }
+        private void DisplayData()
+        {
+            con.Open();
+            DataTable dt = new DataTable();
+            adapter = new SqlDataAdapter("select * from emp", con);
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+
+        //Clear Data
+        private void ClearData()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+
+            textBox3.Text = "";
+            textBox4.Text = "";
+
+
+        }
+
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                cmd = new SqlCommand("delete emp where empNo=@empNo", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@empNo", textBox1.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Record Deleted Successfully!");
+                DisplayData();
+
+                ClearData();
+
+            }
+
+            else
+            {
+                MessageBox.Show("Please Select Record to Delete");
+            }
+        }
+
+        private void btn_upadate_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+            {
+
+                cmd = new SqlCommand("update emp set DeptNo=@DeptNO ,Name=@Name , job=@job ", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@DeptNo", textBox2.Text);
+                // cmd.Parameters.AddWithValue("@empNo", textBox1dno.Text);
+                cmd.Parameters.AddWithValue("@Name", textBox3.Text);
+                cmd.Parameters.AddWithValue("@job", textBox4.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record Updated Successfully");
+
+                con.Close();
+                DisplayData();
+
+            }
+
+
+            else
+            {
+                MessageBox.Show("Please Select Record to Update");
+
+            }
+            ClearData();
+            con.Close();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+
+            textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+
+            textBox4.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            string empNo = textBox5.Text;
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from emp where empNo='" + empNo + "'", con);
+                SqlDataReader reader;
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    textBox1.Text = reader.GetValue(0).ToString();
+                    textBox2.Text = reader.GetValue(1).ToString();
+                    textBox3.Text = reader.GetValue(2).ToString();
+                    textBox4.Text = reader.GetValue(3).ToString();
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error in searching " + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+   
+}
